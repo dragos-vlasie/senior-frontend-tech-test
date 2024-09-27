48005ca2abed897d0265e473d878791b55c7f9ff
dragos-vlasie <37800900+dragos-vlasie@users.noreply.github.com>
2024-09-27 11:43:07 +0100

    update footer data

--- ffern-friends/components/Footer.tsx
+++ ffern-friends/components/Footer.tsx
@@ -1,69 +1,67 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
     <footer className="bg-[#EFE7DA] text-neutral-content p-4 text-center  pt-36">
-        <div className='max-w-6xl mx-auto'>
-
-    <nav className="mb-4 flex justify-evenly">
-      <ul className="menu menu-horizontal text-left">
-        <li>
-          <a href="#" className="hover:text-accent">
-            How it works
+      <div className="max-w-6xl mx-auto">
+        <nav className="mb-4 flex justify-evenly">
+          <ul className="menu menu-horizontal text-left">
+            <li>
+              <a href="#" className="hover:text-accent">
+                How it works
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-accent">
+                Returns
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-accent">
+                FAQ
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-accent">
+                Ledger Portal
+              </a>
+            </li>
+          </ul>
+          <ul className="text-left">
+            <li>
+              <a href="#" className="hover:text-accent">
+                Careers
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-accent">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-accent">
+                Terms and Conditions
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-accent">
+                Press
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex justify-center space-x-4">
+          <a href="#">
+            <p> Facebook </p>
           </a>
-        </li>
-        <li>
-          <a href="#" className="hover:text-accent">
-            Returns
+          <a href="#">
+            <p> Instagram </p>
           </a>
-        </li>
-        <li>
-          <a href="#" className="hover:text-accent">
-            FAQ
+          <a href="#">
+            <p> Twitter </p>
           </a>
-        </li>
-        <li>
-          <a href="#" className="hover:text-accent">
-            Ledger Portal
-          </a>
-        </li>
-      </ul>
-      <ul className='text-left'>
-        <li>
-          <a href="#" className="hover:text-accent">
-            Careers
-          </a>
-        </li>
-        <li>
-          <a href="#" className="hover:text-accent">
-            Privacy Policy
-          </a>
-        </li>
-        <li>
-          <a href="#" className="hover:text-accent">
-            Terms and Conditions
-          </a>
-        </li>
-        <li>
-          <a href="#" className="hover:text-accent">
-            Press
-          </a>
-        </li>
-      </ul>
-    </nav>
-    <div className="flex justify-center space-x-4">
-      <a href="#">
-        <p> Facebook </p>
-      </a>
-      <a href="#">
-        <p> Instagram </p>
-      </a>
-      <a href="#">
-        <p> Twitter </p>
-      </a>
-    </div>
-    </div>
-  </footer>
-
-  )
+        </div>
+      </div>
+    </footer>
+  );
 }
--- ffern-friends/pages/ffern-friend.tsx
+++ ffern-friends/pages/ffern-friend.tsx
@@ -7,18 +7,19 @@ import Layout from "@/components/Layout";
 
 const FfernFriendPage = () => {
   const { data, error, isLoading } = useQuery<GetFfernFriendResponse, Error>({
-    queryKey: ['ffernFriend', 'siobhan-1234'],  
-    queryFn: () => fetchFfernFriend('siobhan-1234'), 
+    queryKey: ["ffernFriend", "siobhan-1234"],
+    queryFn: () => fetchFfernFriend("siobhan-1234"),
   });
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
 
-  console.log("🚀🚀🚀🚀🚀🚀 ~ data:", data)
   return (
     <Layout>
       <div className="text-center mt-48">
-        <h1>{data?.firstName} {data?.lastName}</h1>
+        <h1>
+          {data?.firstName} {data?.lastName}
+        </h1>
         <p>{data?.addressLineOne}</p>
       </div>
     </Layout>
